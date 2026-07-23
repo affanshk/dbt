13f5681e4d89962972215b052b1724cd0a5c7cd9
affanshk <89542890+affanshk@users.noreply.github.com>
2026-07-24 00:16:00 +0500

    Delete macros directory

--- macros/generate_models.sql
+++
@@ -1,7 +0,0 @@
-{% macro model_name(source, table) -%}
-{{ ('staging__' ~ source ~ '__' ~ table).lower() }}
-{%- endmacro %}
-
-{% macro ref_silver(source, table) -%}
-{{ ref(model_name(source, table)) }}
-{%- endmacro %}
